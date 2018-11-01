fff292ffdaf25aa509f73e1d008f13b174ecc63d
Martin Ochoa <martincho8a@gmail.com>
2018-10-31 22:23:46 -0300

    Clase Material

---
+++ app/src/main/java/com/m/nachomorati/gestionstockypedidos/Material.java
@@ -0,0 +1,51 @@
+package com.m.nachomorati.gestionstockypedidos;
+
+public class Material {
+    private int id;
+    private String ear;
+    private String descripcion;
+    private int stock;
+
+    public Material() {
+        this.id = -1;
+    }
+
+    public Material(int id, String ear, String descripcion, int stock) {
+        this.id = id;
+        this.ear = ear;
+        this.descripcion = descripcion;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEar() {
+        return ear;
+    }
+
+    public void setEar(String ear) {
+        this.ear = ear;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
