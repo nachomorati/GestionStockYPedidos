ff39b3905f979d7cb185b745d2b92191d825b268
Martin Ochoa <martincho8a@gmail.com>
2018-11-13 22:47:52 -0300

    agregada fecha de hoy al activity inventario

--- app/src/main/java/com/m/nachomorati/gestionstockypedidos/InventariosActivity.java
+++ app/src/main/java/com/m/nachomorati/gestionstockypedidos/InventariosActivity.java
@@ -3,17 +3,29 @@ package com.m.nachomorati.gestionstockypedidos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class InventariosActivity extends AppCompatActivity {
     ArrayList<Inventario> inventarios = new ArrayList<>();
     ListView lvInventarios;
+    TextView fechaInventario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventarios);
+
+        fechaInventario = (TextView)findViewById(R.id.tvFechaInventario);
+        Date fecha = new Date();
+        String simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy").format(fecha);
+
+        fechaInventario.setText(simpleDateFormat);
 
         //TODO llenar ArrayList con el JSON
         Material m1 = new Material("EAR001100", "RODAMIENTO 6600", 16);
